469efeef205fc6e5765f5bea62dab2b1ce2c9af4
QSX23 <Batuure15@gmail.com>
2025-07-16 15:48:36 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from pynascar!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/schedule.py
@@ -0,0 +1,45 @@
+# src/utils.py
+
+import warnings
+import pandas as pd
+import requests
+
+# endpoint for race list
+#https://cf.nascar.com/cacher/2023/race_list_basic.json
+
+class Races:
+    '''
+    
+    Class to handle fetching and storing race data for a specific year and series ID.
+    Attributes:
+        year (int): The year of the race season.
+        series_id (int): The ID of the NASCAR series (1 for Cup Series, 2 for Xfinity, 3 for Truck Series).
+        
+    '''
+    
+    def __init__(self, year, series_id):
+        self.year = year
+        self.series_id = series_id
+        self.races = []
+        self.data = pd.DataFrame()
+        self.fetch_races()     
+
+    def fetch_races(self):
+        """Fetch the race list for the specified year and series ID."""
+        url = f"https://cf.nascar.com/cacher/{self.year}/race_list_basic.json"
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            # Filter the races by series ID
+            race_list = data[f'series_{self.series_id}']
+            self.races = [race for race in race_list if race['series_id'] == self.series_id]
+            self.data = pd.DataFrame(self.races)
+        else:
+            warnings.warn(f"Failed to fetch race list: {response.status_code}")
+    
+    def get_completed_races(self):
+        """Return a list of completed races."""
+        return self.data[self.data['total_race_time'].isna()]['race_name'].tolist()
+            
+
+    
